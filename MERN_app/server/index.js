6087990e7ef48b888ba59713b710e2764078dcc6
Kevin Tran <320trankt@gmail.com>
2021-10-23 20:46:10 -0700

    kt - fix some deprecated package dependencies

--- MERN_app/server/index.js
+++ MERN_app/server/index.js
@@ -1,12 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
