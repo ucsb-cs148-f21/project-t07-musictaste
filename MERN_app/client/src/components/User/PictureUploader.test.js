5320b01012463f11b52acd881e00748a354b191c
brandonwrson <brandonwrson@gmail.com>
2021-11-10 14:54:24 -0800

    Create PictureUploader.test.js

---
+++ MERN_app/client/src/components/User/PictureUploader.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import PictureUploader, { renderPreview } from "./PictureUploader";
+
+describe("photo uploader", () => {
+    test("see if photo is inserted", () => {
+        const component = render(<PictureUploader />)
+        expect(component.findByAltText("Album Cover"));
+    })
+})
