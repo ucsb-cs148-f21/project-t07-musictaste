74c2b0af1a8980a959011ede8d228a720ad2920d
Brandon Son <brandonwrson@gmail.com>
2021-11-18 23:32:24 -0800

    stuff

--- MERN_app/client/src/components/Navbar/Navbar.js
+++ MERN_app/client/src/components/Navbar/Navbar.js
@@ -19,8 +19,10 @@ const Navbar = () => {
 
     setUser(null);
   };
-
-  const blog = () => {
+  
+  const openUser = () => history.push(`/users/${user?.result?._id}`);
+  // Need to handle when user is null -> lead to 404 error 
+  const blog = () => {  
     history.push("/posts");
   };
   useEffect(() => {
@@ -90,8 +92,7 @@ const Navbar = () => {
         <Avatar
           src={avataricon}
           variant="contained"
-          component={Link}
-          to="/user"
+          onClick = {openUser}
         >
           User Profile
         </Avatar>
